cdff995bc4764215a09cb483174a9500ac29f000
wallymathieu <gewalli@gmail.com>
2011-04-20 18:34:00 +0200

    Added a test Console project

---
+++ Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(string.Join(Environment.NewLine, args));
+        }
+    }
+}
