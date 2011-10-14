3fe026afaf1ddd3ca8d2660db880f4dfc3b42ac9
Oskar Gewalli <gewalli@gmail.com>
2011-10-14 20:49:52 +0200

    Changed assembly version to 1.2

--- Isop/AssemblyInfo.cs
+++ Isop/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("Isop")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Command line helper library.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyVersion("1.2.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
