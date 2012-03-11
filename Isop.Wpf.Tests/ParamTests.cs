b977a5a5e7f3193ace2704cd3b5abe0029f4427f
wallymathieu <gewalli@gmail.com>
2012-03-11 16:13:38 +0100

    Isop wpf tests.

---
+++ Isop.Wpf.Tests/ParamTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Isop.WpfControls.ViewModels;
+using System.ComponentModel;
+
+namespace Isop.Wpf.Tests
+{
+    [TestFixture]
+    public class ParamTests
+    {
+        [Test]
+        public void Changing_the_value_of_param_will_trigger_callback() 
+        {
+            var p = new Param(typeof(string), "name", new ArgumentWithOptions(new ArgumentParameter("name", new []{"name"})));
+            bool triggered = false;
+            p.PropertyChanged += (s, arg) => { triggered = true; };
+            p.Value = "val";
+            Assert.That(triggered);
+        }
+        [Test]
+        public void Changing_the_value_of_param_without_callback()
+        {
+            var p = new Param(typeof(string), "name", new ArgumentWithOptions(new ArgumentParameter("name", new[] { "name" })));
+            p.Value = "val";
+        }
+    }
+}
