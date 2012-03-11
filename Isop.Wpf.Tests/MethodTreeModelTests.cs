8df28688f32f42106140d5cfa51e5c73999dbb72
wallymathieu <gewalli@gmail.com>
2012-03-11 17:08:31 +0100

    Added tests of treemodel propertychanged behaviour.

--- Isop.Wpf.Tests/MethodTreeModelTests.cs
+++ Isop.Wpf.Tests/MethodTreeModelTests.cs
@@ -22,7 +22,7 @@ namespace Isop.Wpf.Tests
         }
         private class MyController
         {
-            public string Action() { return null; }
+            public string Action(string name) { return null; }
         }
         [Test]
         public void Can_generate_tree_model_from_configuration_with_controllers()
@@ -32,6 +32,32 @@ namespace Isop.Wpf.Tests
                 .GetMethodTreeModel();
             Assert.That(treemodel.Controllers.Count(), Is.EqualTo(1));
             Assert.That(treemodel.GlobalParameters.Count(), Is.EqualTo(0));
+        }
+        [Test]
+        public void When_setting_value_on_global_parameter_will_set_value_on_method_parameter()
+        {
+            var treemodel = TreeModelWithCurrentMethodSelected();
+            treemodel.GlobalParameters.First().Value = "new value";
+            Assert.That(treemodel.CurrentMethod.Parameters.Single().Value, 
+                Is.EqualTo("new value"));
+        }
+        [Test]
+        public void When_setting_value_on_method_parameter_will_set_value_on_global_parameter()
+        {
+            var treemodel = TreeModelWithCurrentMethodSelected();
+            treemodel.CurrentMethod.Parameters.Single().Value = "new value";
+            Assert.That(treemodel.GlobalParameters.First().Value,
+                Is.EqualTo("new value"));
+        }
+        private static MethodTreeModel TreeModelWithCurrentMethodSelected()
+        {
+            var treemodel = new Build()
+                .Parameter(
+                    new ArgumentParameter("name", new[] { "name" }))
+                .Recognize(new MyController())
+                .GetMethodTreeModel();
+            treemodel.CurrentMethod = treemodel.Controllers.Single().Methods.Single();
+            return treemodel;
         }
     }
 }
