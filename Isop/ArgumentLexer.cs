b914f9f9bd7088dd75eff5423b9e60848f6b20ac
wallymathieu <gewalli@gmail.com>
2012-11-18 13:09:33 +0100

    Updated lexer based on clojure code.
    The clojure code was more readable, thus a c# variant is written.

--- Isop/ArgumentLexer.cs
+++ Isop/ArgumentLexer.cs
@@ -7,58 +7,60 @@ using System.Text.RegularExpressions;
 
 namespace Isop
 {
-    
     public class ArgumentLexer : List<Token>
     {
         private static readonly Regex ParamPattern = new Regex("(?<paramPrefix>--|/|-)(?<param>[^:=]*)([:=]?)(?<paramValue>.*)");
-        
+
         public ArgumentLexer(IEnumerable<string> arg)
-            :this(Lex(arg.ToList()))
+            : this(Lex(arg.ToList()))
         {
         }
+
         public ArgumentLexer(IEnumerable<Token> tokens)
-            :base(tokens)
+            : base(tokens)
         {
+        }
+
+        private static Token mapParametervaluesBasedOnParameters(Token? previous, Token token)
+        {
+            if (null != previous && previous.Value.TokenType == TokenType.Parameter && token.TokenType == TokenType.Argument)
+            {
+                return new Token(token.Value, TokenType.ParameterValue, token.Index);
+            }
+            return token;
+        }
+
+        private static IEnumerable<Token> MapTokensFromRaw(string arg, int valueIndex)
+        {
+            var match = ParamPattern.Match(arg);
+            if (match.Success)
+            {
+                yield return new Token(match.Groups["param"].Value, TokenType.Parameter, valueIndex);
+                if (match.Groups["paramValue"].Length > 0)
+                {
+                    yield return new Token(match.Groups["paramValue"].Value, TokenType.ParameterValue, valueIndex);
+                }
+            }
+            else
+            {
+                yield return new Token(arg, TokenType.Argument, valueIndex);
+            }
+        }
+
+        private static IEnumerable<T> SelectWithPrevious<T>(Func<T?, T, T> map, IEnumerable<T> sequence) where T : struct
+        {
+            T? last = null;
+            foreach (T item in sequence)
+            {
+                yield return map(last, item);
+                last = item;
+            }
         }
 
         public static IEnumerable<Token> Lex(IList<string> arg)
         {
-            var currentIndex = 0;
-            var length = arg.Count();
-            while (currentIndex<length) 
-            {
-                var value = arg[currentIndex];
-                var valueIndex = currentIndex;
-                currentIndex++;
-                
-                var match = ParamPattern.Match(value);
-                if (match.Success)
-                {
-                    yield return new Token(match.Groups["param"].Value, TokenType.Parameter, valueIndex);
-                    if (match.Groups["paramValue"].Length > 0)
-                    {
-                        yield return new Token(match.Groups["paramValue"].Value, TokenType.ParameterValue, valueIndex);
-                    }
-                    else
-                    {
-                        if (currentIndex < length)
-                        {
-                            var possibleParamValue = arg[currentIndex];
-                            var possibleParamValueIndex = currentIndex;
-                            if (!ParamPattern.IsMatch(possibleParamValue))
-                            {
-                                currentIndex++;
-                                yield return new Token(possibleParamValue, TokenType.ParameterValue, possibleParamValueIndex);
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    yield return new Token(value, TokenType.Argument, valueIndex);
-                }
-            }
+            var firstSweep = arg.SelectMany((value, valueIndex) => MapTokensFromRaw(value, valueIndex));
+            return SelectWithPrevious<Token>(mapParametervaluesBasedOnParameters, firstSweep);
         }
     }
-    
 }
