07fd06a8c8cee72eb57f834dfcbdf8fa9737c7e0
Oskar Gewalli <gewalli@gmail.com>
2015-01-24 17:31:38 +0100

    Should handle different configurations

--- src/Tests/ConfigurationTests.cs
+++ src/Tests/ConfigurationTests.cs
@@ -12,47 +12,59 @@ namespace Isop.Tests
     [TestFixture]
     public class ConfigurationTests
     {
-        [Test] public void Can_dispose_of_configuration_after_usage()
+        [Test]
+        public void Can_dispose_of_configuration_after_usage()
         {
             var conf = new FullConfiguration();
             var parserBuilder = new Build().Configuration(conf);
             parserBuilder.Dispose();
             Assert.That(conf.DisposeCalled);
         }
-        
-        [Test] public void Can_read_configuration_from_example_project()
+
+        private string TryGetExamplePath()
         {
-            var path = Path.GetFullPath( Path.Combine("..","..","..",
-                Path.Combine("Example","bin","Debug")));
-            
+            return new string[] { "Debug", "Release" }.Select(configuration=>
+                Path.GetFullPath(Path.Combine("..", "..", "..",
+                    Path.Combine("Example", "bin", configuration)))
+            ).FirstOrDefault(path=>Directory.Exists(path));
+        }
+
+        [Test]
+        public void Can_read_configuration_from_example_project()
+        {
+            var path = TryGetExamplePath();
+
             var parserBuilder = new Build().ConfigurationFrom(path);
 
-            Assert.That(parserBuilder.ControllerRecognizers.Count(), 
+            Assert.That(parserBuilder.ControllerRecognizers.Count(),
                 Is.AtLeast(2));
         }
-        
-        [Test] public void Can_invoke_configure_method_on_configuration()
+
+        [Test]
+        public void Can_invoke_configure_method_on_configuration()
         {
             var conf = new FullConfiguration();
             var parserBuilder = new Build().Configuration(conf);
-            var parsed = parserBuilder.Parse(new []{"--global","globalvalue","--globalrequired","2","My","Action","--value","1"});
+            var parsed = parserBuilder.Parse(new[] { "--global", "globalvalue", "--globalrequired", "2", "My", "Action", "--value", "1" });
             var cout = new StringWriter();
             parsed.Invoke(cout);
-            Assert.That(conf.Global,Is.EqualTo("globalvalue"));
+            Assert.That(conf.Global, Is.EqualTo("globalvalue"));
         }
 
-        [Test] public void Can_read_documentation_for_properties()
+        [Test]
+        public void Can_read_documentation_for_properties()
         {
             var conf = new FullConfiguration();
             var parserBuilder = new Build().Configuration(conf);
             var globalDesc = parserBuilder.GlobalParameters
-                .First(gp=>gp.Argument.Prototype.Equals("Global")).Description;
-            Assert.That(globalDesc,Is.EqualTo("GLOBAL!!"));
+                .First(gp => gp.Argument.Prototype.Equals("Global")).Description;
+            Assert.That(globalDesc, Is.EqualTo("GLOBAL!!"));
         }
-        [Test] public void Can_use_autoconfiguration()
+        [Test]
+        public void Can_use_autoconfiguration()
         {
             var recognizes = new AssemblyScanner(this.GetType().Assembly).LooksLikeControllers().ToArray();
-            Assert.That(recognizes,Is.EquivalentTo(new []{typeof(MyController)}));
+            Assert.That(recognizes, Is.EquivalentTo(new[] { typeof(MyController) }));
         }
     }
 }
