4f85354e6c555e6a68992bc9e1d314e0e17405e4
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2019-07-15 20:31:46 +0200

    Trying things out

--- src/Example/CustomerController.cs
+++ src/Example/CustomerController.cs
@@ -1,10 +1,10 @@
+using System.Threading.Tasks;
+
 namespace Example
 {
     public class CustomerController
     {
-        public string Add(string name)
-        {
-            return "invoking action Add on customercontroller with name : " + name;
-        }
+        public Task<string> Add(string name) =>
+            Task.FromResult("invoking action Add on customer controller with name : " + name);
     }
 }
