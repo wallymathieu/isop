cc849be501153aa7fd05dd08a38f867aa145f5f8
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2019-07-20 21:45:39 +0200

    Refactoring test

--- src/Tests/Given_controller_with_file_argument.cs
+++ src/Tests/Given_controller_with_file_argument.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Isop;
@@ -15,48 +16,61 @@ namespace Tests
             public Func<FileStream, string> OnAction { get; set; }
             public string Action(FileStream file) { return OnAction(file); }
         }
-        [Test]
-        public void It_can_handle_file_argument()
+
+        class DeleteOnDispose:IDisposable
         {
-            var filename = string.Empty;
+            public List<FileStream> files { get; }=new List<FileStream>();
 
-            var sc = new ServiceCollection();
-            sc.AddSingleton(ci => new MyFileController() { OnAction = (file) => filename = file.Name });
-
-
-            FileStream fileStream = null;
-            try
+            public void Dispose()
             {
-
-                var arguments = Builder.Create(sc)
-                        
-                    .SetTypeConverter((t, s, c) =>
-                    {
-
-                        fileStream = new FileStream(s, FileMode.Create);
-                        return fileStream;
-                    })
-                    //Need to set type converter 
-                    .Recognize(typeof(MyFileController))
-                    .BuildAppHost()
-                    .Parse(new[] { "MyFile", "Action", "--file", "myfile.txt" });
-
-                Assert.That(arguments.Unrecognized.Select(u=>u.Value).ToArray(), Is.Empty);
-                arguments.Invoke(new StringWriter());
-                Assert.True(filename.Contains("myfile.txt"));
-            }
-            finally
-            {
-                if (null != fileStream)
+                foreach (var fileStream in files)
                 {
+                    var name = fileStream.Name;
                     try
                     {
                         fileStream.Dispose();
                     }
-                    // ReSharper disable EmptyGeneralCatchClause
-                    catch { }
-                    // ReSharper restore EmptyGeneralCatchClause
+                    catch (Exception ex)
+                    {
+                        Console.Error.WriteLine($"Tried to dispose fileStream {ex.Message}");
+                    }
+
+                    if (!File.Exists(name)) continue;
+                    try
+                    {
+                        File.Delete(name);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Error.WriteLine($"Tried to delete file {ex.Message}");
+                    }
                 }
+            }
+        }
+        
+        [Test]
+        public void It_can_handle_file_argument()
+        {
+            var filename = string.Empty;
+            var sc = new ServiceCollection();
+            sc.AddSingleton(ci => new MyFileController { OnAction = file => filename = file.Name });
+            using (var files = new DeleteOnDispose())
+            {
+                var arguments = Builder.Create(sc)
+                    .SetTypeConverter((t, s, c) =>
+                    {
+                        //creating file before use ...
+                        var fileStream = new FileStream(s, FileMode.Create);
+                        files.files.Add(fileStream);
+                        return fileStream;
+                    })
+                    .Recognize(typeof(MyFileController))
+                    .BuildAppHost()
+                    .Parse(new[] {"MyFile", "Action", "--file", "myfile.txt"});
+
+                Assert.That(arguments.Unrecognized.Select(u => u.Value).ToArray(), Is.Empty);
+                arguments.Invoke(new StringWriter());
+                Assert.True(filename.Contains("myfile.txt"));
             }
         }
 
